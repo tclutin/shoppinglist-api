3639fec6f7d6dc6bd2d03f61996d6a7f207e3b59
Lutin <tclutin01@gmail.com>
2025-01-02 17:24:19 +0500

    add: base structure of config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,29 @@
 package config
 
+import "time"
+
 type Config struct {
+	HTTPServer HTTPServer
+	Postgres   Postgres
+	JWT        JWT
+}
+
+type HTTPServer struct {
+	Host string
+	Port string
+}
+
+type Postgres struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+type JWT struct {
+	Secret string
+	Expire time.Duration
 }
 
 func MustLoad() *Config {

7468e64976fd1b3f52b9b155703d4b79c42c557b
Lutin <tclutin01@gmail.com>
2025-01-04 03:29:38 +0500

    add: base logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,11 +1,27 @@
 package logger
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+)
 
-type Logger struct {
-	IsProd bool
-	logger *slog.Logger
-}
+func New(isProd bool) *slog.Logger {
+	var logger *slog.Logger
 
-func New() *slog.Logger {
+	if isProd {
+		optsProd := &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, optsProd))
+
+	}
+
+	if !isProd {
+		optsDev := &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}
+		logger = slog.New(slog.NewTextHandler(os.Stdout, optsDev))
+	}
+
+	return logger
 }

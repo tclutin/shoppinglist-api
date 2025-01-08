a6a11ae406a52651a26ffca3aee6fe7393ccb988
Lutin <tclutin01@gmail.com>
2025-01-09 04:57:32 +0500

    ref: errors.

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -3,21 +3,45 @@ package errors
 import "errors"
 
 var (
-	ErrMissingCredentials  = errors.New("missing credentials")
-	ErrUserAlreadyExists   = errors.New("username already exists")
-	ErrUserNotValid        = errors.New("username/password is not correct")
-	ErrSessionNotFound     = errors.New("session not found")
+	// ErrMissingCredentials AuthService
+	ErrMissingCredentials = errors.New("missing credentials")
+
+	// ErrUserAlreadyExists AuthService
+	ErrUserAlreadyExists = errors.New("username already exists")
+
+	// ErrUserNotValid AuthService
+	ErrUserNotValid = errors.New("username/password is not correct")
+
+	// ErrSessionNotFound AuthService
+	ErrSessionNotFound = errors.New("session not found")
+
+	// ErrRefreshTokenExpired AuthService
 	ErrRefreshTokenExpired = errors.New("refresh token expired")
 
+	// ErrUserNotFound UserService
 	ErrUserNotFound = errors.New("user not found")
 
-	ErrInvalidCode        = errors.New("invalid code")
-	ErrAlreadyMember      = errors.New("already a member")
-	ErrGroupNotFound      = errors.New("group not found")
-	ErrMemberNotFound     = errors.New("member not found")
-	ErrAreNotOwner        = errors.New("you aren't owner")
-	ErrOwnerCannotLeave   = errors.New("owner cannot leave")
+	// ErrInvalidCode GroupService
+	ErrInvalidCode = errors.New("invalid code")
+
+	// ErrAlreadyMember GroupService
+	ErrAlreadyMember = errors.New("already a member")
+
+	// ErrGroupNotFound GroupService
+	ErrGroupNotFound = errors.New("group not found")
+
+	// ErrMemberNotFound GroupService
+	ErrMemberNotFound = errors.New("member not found")
+
+	// ErrAreNotOwner GroupService
+	ErrAreNotOwner = errors.New("you aren't owner")
+
+	// ErrOwnerCannotLeave GroupService
+	ErrOwnerCannotLeave = errors.New("owner cannot leave")
+
+	// ErrCannotKickYourself GroupService
 	ErrCannotKickYourself = errors.New("you can not kick yourself")
 
+	// ErrProductNotFound ProductService
 	ErrProductNotFound = errors.New("product not found")
 )
